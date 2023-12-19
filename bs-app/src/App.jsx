07d0932a0256268eee29bf326bf58a1aef553793
MelissaCanu <melissamcanu@gmail.com>
2023-12-19 20:58:33 +0100

    created welcomecomponent

--- bs-app/src/App.jsx
+++ bs-app/src/App.jsx
@@ -3,11 +3,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import MyNav from "./components/MyNav";
 import MyFooter from "./components/myFooter";
+import Welcome from "./components/Welcome";
 
 function App() {
 	return (
 		<div className="App">
 			<MyNav />
+			<Welcome name="GattoRude" fontSize="fs-2" textSize="fs-3" />
 			<MyFooter />
 		</div>
 	);

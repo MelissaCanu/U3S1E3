a38daad19346dc389d8e52de541e41e983d81611
MelissaCanu <melissamcanu@gmail.com>
2023-12-20 16:31:40 +0100

    implemented react-bs container, row, col

--- bs-app/src/components/BookList.jsx
+++ bs-app/src/components/BookList.jsx
@@ -1,13 +1,18 @@
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 import SingleBook from "./SingleBook";
 
 const BookList = ({ books }) => {
 	return (
-		<div>
-			{books.map((book) => (
-				<SingleBook key={book.asin} book={book} />
-			))}
-		</div>
+		<Container>
+			<Row>
+				{books.map((book) => (
+					<Col className="mb-3" xs={6} xl={2} key={book.asin}>
+						<SingleBook book={book} />
+					</Col>
+				))}
+			</Row>
+		</Container>
 	);
 };
 

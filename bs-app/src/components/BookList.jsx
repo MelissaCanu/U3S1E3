54278a435ffac49f1e5b3dac256fbfb010b1cbd1
MelissaCanu <melissamcanu@gmail.com>
2023-12-20 18:10:29 +0100

    transformed booklist into class  component

--- bs-app/src/components/BookList.jsx
+++ bs-app/src/components/BookList.jsx
@@ -1,20 +1,41 @@
-import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Component } from "react";
+import { Row, Col, Form } from "react-bootstrap";
 import SingleBook from "./SingleBook";
 
-const BookList = ({ books }) => {
-	return (
-		<Container>
+class BookList extends Component {
+	state = {
+		searchQuery: "",
+	};
+
+	handleChange = (event) => {
+		this.setState({ searchQuery: event.target.value });
+	};
+	render() {
+		const { books } = this.props;
+		const { searchQuery } = this.state;
+
+		return (
 			<Row>
+				<Col xs={12}>
+					<Form.Control
+						type="text"
+						placeholder="Search book here"
+						value={searchQuery}
+						onChange={this.handleChange}
+					/>
+				</Col>
 				{books.map((book) => (
 					<Col className="mb-3" xs={6} xl={2} key={book.asin}>
 						<SingleBook book={book} />
 					</Col>
 				))}
 			</Row>
-		</Container>
-	);
-};
+		);
+	}
+}
 
 export default BookList;
-/* adding comment to try commit to github */
+
+/* props.categoryArray
+        .filter((book) => book.title.toLowerCase().includes(props.valueToSearch.toLowerCase()))
+        .map((book, index) => */

9774aac97e82e51ce5349aeba8807b7d2ef9cfce
MelissaCanu <melissamcanu@gmail.com>
2023-12-21 14:32:56 +0100

    corrections made, filter works

--- bs-app/src/App.jsx
+++ bs-app/src/App.jsx
@@ -6,19 +6,22 @@ import MyFooter from "./components/myFooter";
 import Welcome from "./components/Welcome";
 // import AllTheBooks from "./components/AllTheBooks";
 /* ------------------------------------------------ */
+import { Container } from "react-bootstrap";
+import BookList from "./components/BookList";
 import horror from "./data/horror.json";
 // import SingleBook from "./components/SingleBook";
-import BookList from "./components/BookList";
 
 function App() {
 	// const staticHorrorBook = horror[4];
 	return (
 		<div className="App">
 			<MyNav />
-			<Welcome name="GattoRude" fontSize="fs-2" textSize="fs-3" />
-			{/* <AllTheBooks /> */}
-			{/* <SingleBook book={staticHorrorBook} /> */}
-			<BookList books={horror} />
+			<Container>
+				<Welcome name="GattoRude" fontSize="fs-2" textSize="fs-3" />
+				{/* <AllTheBooks /> */}
+				{/* <SingleBook book={staticHorrorBook} /> */}
+				<BookList books={horror} />
+			</Container>
 			<MyFooter />
 		</div>
 	);
--- bs-app/src/components/BookList.jsx
+++ bs-app/src/components/BookList.jsx
@@ -7,12 +7,12 @@ class BookList extends Component {
 		searchQuery: "",
 	};
 
-	handleChange = (event) => {
-		this.setState({ searchQuery: event.target.value });
-	};
+	// handleChange = (event) => {
+	// 	this.setState({ searchQuery: event.target.value });
+	// };
 	render() {
-		const { books } = this.props;
-		const { searchQuery } = this.state;
+		// const { books } = this.props;
+		// const { searchQuery } = this.state;
 		// const filteredBooks = books.filter((book) =>
 		// 	book.title.toLowerCase().includes(searchQuery.toLowerCase())
 		// );
@@ -20,21 +20,31 @@ class BookList extends Component {
 		/* funziona in console, filtra in base all'input, ma non renderizza sulla pagina */
 
 		return (
-			<Row className="mx-2">
-				<Col className="mb-3" xs={12}>
-					<Form.Control
-						type="text"
-						placeholder="Search book here"
-						value={searchQuery}
-						onChange={this.handleChange}
-					/>
-				</Col>
-				{books.map((book) => (
-					<Col className="mb-3" xs={6} xl={2} key={book.asin}>
-						<SingleBook book={book} />
+			<>
+				<Row className="mx-2">
+					<Col className="mb-3" xs={12}>
+						<Form.Group>
+							<Form.Control
+								type="search"
+								placeholder="Search book here"
+								value={this.state.searchQuery}
+								onChange={(e) => this.setState({ searchQuery: e.target.value })}
+							/>
+						</Form.Group>
 					</Col>
-				))}
-			</Row>
+				</Row>
+				<Row>
+					{this.props.books
+						.filter((book) =>
+							book.title.toLowerCase().includes(this.state.searchQuery)
+						)
+						.map((book) => (
+							<Col className="mb-3" xs={6} xl={2} key={book.asin}>
+								<SingleBook book={book} />
+							</Col>
+						))}
+				</Row>
+			</>
 		);
 	}
 }
